e6f345184f7450c292eea1548efffb1f819b9655
Eduardo Evaristo <eduardo.evaristo.am@gmail.com>
2024-11-08 11:19:07 -0300

    refactor: convert socket intructions to functions to improve modularity

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,20 +1,44 @@
+import { RawData, WebSocket } from "ws";
 import server from "./server";
+import { IncomingMessage } from "http";
 
 //Initialize array of connected sockets
-let sockets = [];
+let sockets: WebSocket[] = [];
 
 //Whenever our server gets a connection, we take the socket that connected and
-server.on("connection", (socket) => {
+server.on("connection", (socket: WebSocket, req: IncomingMessage) => {
   //Add it to our list of known sockets connections
-  sockets.push(socket);
+  connectSocket(socket);
+  console.log(`Socket: ${req.socket.remoteAddress} has connected.`);
 
   //What do to when our server gets a message from said socket
-  socket.on("message", (data) => {
-    //Turns buffer back into string/JSON
-    const senhas = data.toLocaleString();
-    //Share message to each connected socket
-    sockets.forEach((socket) => {
-      socket.send(senhas);
-    });
+  socket.on("message", (data: RawData) => {
+    //Turns buffer/raw data back into string/JSON
+    const message: string = data.toLocaleString();
+    //Share message to each connected socket but the sender
+    sendMessage(socket, message);
+  });
+
+  //Removing socket from array whenever it closes the connection
+  socket.on("close", () => {
+    //Filter socket out of sockets
+    closeSocket(socket);
+    console.log(`Socket: ${req.socket.remoteAddress} has been disconnected.`);
   });
 });
+
+//Functions
+function sendMessage(senderSocket: WebSocket, message: string) {
+  sockets.forEach((socket) => {
+    if (socket === senderSocket) return;
+    socket.send(message);
+  });
+}
+
+function closeSocket(toBeRemovedSocket: WebSocket) {
+  sockets = sockets.filter((socket) => socket !== toBeRemovedSocket);
+}
+
+function connectSocket(socket: WebSocket) {
+  sockets.push(socket);
+}
